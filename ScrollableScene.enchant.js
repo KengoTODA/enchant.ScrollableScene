b2714e95390682cc74bbc4f5caebc6f07321c588
eller86 <skypencil@gmail.com>
2011-12-24 12:01:58 +0800

    add scroll bar

--- ScrollableScene.enchant.js
+++ ScrollableScene.enchant.js
@@ -19,6 +19,14 @@ enchant.ScrollableScene = enchant.Class.create(enchant.Scene, {
 		WHEEL_SPEED = 30,
 		that = this;
 		this.height = 0;
+		this._scrollBar = document.createElement('div');
+		this._scrollBar.style.position = 'fixed';
+		this._scrollBar.style.overflow = 'hidden';
+		this._scrollBar.style.backgroundColor = '#cccccc';
+		this._scrollBar.style.width = '4px';
+		this._scrollBar.style.top = '0px';
+		this._scrollBar.style.right = '2px';
+		this._element.appendChild(this._scrollBar);
 
 		this._element.addEventListener(EVENT_NAMES[0],
 			function (e) {
@@ -31,7 +39,7 @@ enchant.ScrollableScene = enchant.Class.create(enchant.Scene, {
 				if (typeof that.dragStart !== 'undefined') {
 					e.preventDefault();
 					that.y += e.pageY - that.dragStart;
-					that.y = Math.min(0, Math.max(that.y, enchant.Game.instance.height - that.height));
+					that._fixPosition();
 					that.dragStart = e.pageY;
 				}
 			},
@@ -58,7 +66,7 @@ enchant.ScrollableScene = enchant.Class.create(enchant.Scene, {
         			return delta;
 				}
 				that.y += optimizeWheel(e) * WHEEL_SPEED;
-				that.y = Math.min(0, Math.max(that.y, enchant.Game.instance.height - that.height));
+				that._fixPosition();
 			},
 			false
 		);
@@ -66,25 +74,32 @@ enchant.ScrollableScene = enchant.Class.create(enchant.Scene, {
 
 	addChild: function (node) {
 		enchant.Scene.prototype.addChild.call(this, node);
-		this.height = this.calcHeight(this.childNodes);
+		this.height = this._calcHeight(this.childNodes);
 	},
 
 	insertBefore: function (node, reference) {
 		enchant.Scene.prototype.insertBefore.call(this, node, reference);
-		this.height = this.calcHeight(this.childNodes);
+		this.height = this._calcHeight(this.childNodes);
 	},
 
 	removeChild: function (node) {
 		enchant.Scene.prototype.removeChild.call(this, node);
-		this.height = this.calcHeight(this.childNodes);
+		this.height = this._calcHeight(this.childNodes);
 	},
 
-	calcHeight: function (children) {
-		var i, length, result = 0, child;
+	_fixPosition: function () {
+		var gameHeight = enchant.Game.instance.height;
+		this.y = Math.min(0, Math.max(this.y, enchant.Game.instance.height - this.height));
+		this._scrollBar.style.top = ((gameHeight - this._scrollBar.height) * (this.y / (this.height - gameHeight))) + 'px';
+	},
+
+	_calcHeight: function (children) {
+		var i, length, result = 0, child, gameHeight = enchant.Game.instance.height;
 		for (i = 0, length = children.length; i < length; ++i) {
 			child = children[i];
 			result = Math.max(result, child.y + (child.height || 0));
 		}
+		this._scrollBar.style.height = Math.round(gameHeight * gameHeight / result) + 'px';
 		return result;
 	}
 });
